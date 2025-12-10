9f5d7d32e1a4d4087df888625d20a48289bf9eb1
mndev2019 <muskanvision2019@gmail.com>
2025-12-10 17:05:53 +0530

    change

--- src/Pages/CrmProcessManagement/Activities/DocumentGeneration.jsx
+++ src/Pages/CrmProcessManagement/Activities/DocumentGeneration.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Topnav from "../../../Component/Topnav";
-import newlead from "../../../assets/Images/Contract.png";
+import newlead from "../../../assets/Images/contract.png";
 import { FiSearch } from "react-icons/fi";
 import { HiOutlineDotsVertical } from "react-icons/hi";
 import { FaRegPlusSquare } from "react-icons/fa";
